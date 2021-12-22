c90ff855ebb65fbf720b58191aedf39f88752b99
root <root@localhost.localdomain>
2021-12-22 20:28:46 +0100

    add led and beep

--- issue.sh
+++ issue.sh
@@ -6,6 +6,12 @@ sleep 15
 ip=$(hostname -I | awk '{print $1}')
 host=$(hostname)
 
+# Enable leds for apu boards
+if ls "/sys/class/leds/apu:green:1/trigger" > /dev/null ; then
+echo 'cpu' > /sys/class/leds/apu:green:1/trigger
+echo 'disk-activity' > /sys/class/leds/apu:green:2/trigger
+fi
+
 echo "
 ---- Welcome to PBS Apliance ----
 Host IP:  $ip
